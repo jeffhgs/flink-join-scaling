b8c6163c449bceaf6d7ae53b0abf66bedbe70a40
Jeff Henrikson <jeff@groovescale.com>
2020-01-25 18:50:36 -0800

    fix: ktFromX matching incorrectly

--- src/test/scala/flink_perf/JoinSpec.scala
+++ src/test/scala/flink_perf/JoinSpec.scala
@@ -27,7 +27,7 @@ class JoinSpec extends AnyFunSuite {
     ab match {
       case (Some(a),Some(b)) => (a.id.toString,Math.max(a.ts, b.ts))
       case (Some(a),None) => (a.id.toString,a.ts)
-      case (Some(a),Some(b)) => (b.id.toString,b.ts)
+      case (None,Some(b)) => (b.id.toString,b.ts)
       case _ => throw new RuntimeException("Internal error")
     }
   }
