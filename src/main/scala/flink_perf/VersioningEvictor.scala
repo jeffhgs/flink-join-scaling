2587858c32058194993aac306305f2333ec97327
Jeff Henrikson <jeff@groovescale.com>
2020-05-29 09:47:58 -0700

    refactor: extract method deduplicateMutable

--- src/main/scala/flink_perf/VersioningEvictor.scala
+++ src/main/scala/flink_perf/VersioningEvictor.scala
@@ -24,52 +24,9 @@ class VersioningEvictor[X,Y](
                            window: GlobalWindow,
                            evictorContext: Evictor.EvictorContext): Unit =
   {
-    if((size+1) % numRecordsBetweenCleanup > 0) {
-      return
-    }
-    val newestX = mutable.Map[String,Long]()
-    val newestY = mutable.Map[String,Long]()
-    val iter1 = elements.iterator()
-    while(iter1.hasNext()) {
-      val e = iter1.next().getValue
-      if(e.isOne()) {
-        val x = e.getOne()
-        val id = idFromX(x)
-        val tsOld = newestX.getOrElseUpdate(id, tsFromX(x))
-        val ts = tsFromX(x)
-        if(ts > tsOld) {
-          newestX.update(id,ts)
-        }
-      } else {
-        val y = e.getTwo()
-        val id = idFromY(y)
-        val tsOld = newestY.getOrElseUpdate(id, tsFromY(y))
-        val ts = tsFromY(y)
-        if(ts > tsOld) {
-          newestY.update(id,ts)
-        }
-      }
-    }
-    val iter2 = elements.iterator()
-    while(iter2.hasNext()) {
-      val e = iter2.next().getValue
-      if(e.isOne()) {
-        val x = e.getOne()
-        val id = idFromX(x)
-        val tsMax = newestX.getOrElse(id,Long.MinValue)
-        val ts = tsFromX(x)
-        if(ts < tsMax) {
-          iter2.remove()
-        }
-      } else {
-        val y = e.getTwo()
-        val id = idFromY(y)
-        val tsMax = newestX.getOrElse(id,Long.MinValue)
-        val ts = tsFromY(y)
-        if(ts < tsMax) {
-          iter2.remove()
-        }
-      }
+    if ((size + 1) % numRecordsBetweenCleanup == 0) {
+      versionDeduplicator.deduplicateMutable(idFromX, idFromY, tsFromX, tsFromY, elements)
     }
   }
+
 }
--- src/main/scala/flink_perf/versionDeduplicator.scala
+++ src/main/scala/flink_perf/versionDeduplicator.scala
@@ -1,5 +1,10 @@
 package flink_perf
 
+import java.lang
+
+import org.apache.flink.streaming.api.datastream.CoGroupedStreams
+import org.apache.flink.streaming.runtime.operators.windowing.TimestampedValue
+
 import scala.collection.mutable
 import collection.JavaConverters._
 
@@ -44,5 +49,56 @@ object versionDeduplicator {
         v._2.update(idy, y)
     }
     mp.values.map(v => (v._1.values, v._2.values))
+  }
+
+  def deduplicateMutable[X,Y](
+                                  idFromX: X => String, idFromY: Y => String, tsFromX: X => Long, tsFromY: Y => Long,
+                                  elements: lang.Iterable[TimestampedValue[CoGroupedStreams.TaggedUnion[X, Y]]]): Unit
+  = {
+    val newestX = mutable.Map[String, Long]()
+    val newestY = mutable.Map[String, Long]()
+    val iter1 = elements.iterator()
+    while (iter1.hasNext()) {
+      val e = iter1.next().getValue
+      if (e.isOne()) {
+        val x = e.getOne()
+        val id = idFromX(x)
+        val tsOld = newestX.getOrElseUpdate(id, tsFromX(x))
+        val ts = tsFromX(x)
+        if (ts > tsOld) {
+          newestX.update(id, ts)
+        }
+      } else {
+        val y = e.getTwo()
+        val id = idFromY(y)
+        val tsOld = newestY.getOrElseUpdate(id, tsFromY(y))
+        val ts = tsFromY(y)
+        if (ts > tsOld) {
+          newestY.update(id, ts)
+        }
+      }
+    }
+    val iter2 = elements.iterator()
+    while (iter2.hasNext()) {
+      val e = iter2.next().getValue
+      if (e.isOne()) {
+        val x = e.getOne()
+        val id = idFromX(x)
+        val tsMax = newestX.getOrElse(id, Long.MinValue)
+        val ts = tsFromX(x)
+        if (ts < tsMax) {
+          iter2.remove()
+        }
+      } else {
+        val y = e.getTwo()
+        val id = idFromY(y)
+        val tsMax = newestX.getOrElse(id, Long.MinValue)
+        val ts = tsFromY(y)
+        if (ts < tsMax) {
+          iter2.remove()
+        }
+      }
+    }
+    false
   }
 }
